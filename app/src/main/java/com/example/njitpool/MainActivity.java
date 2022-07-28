688d2527ae189a29d01d26aa644b958ed51e1559
justinnietzer <justinnietzer@gmail.com>
2022-07-27 22:24:39 -0400

    Feature: Completed Login/Regis for Driver and Customer

--- app/src/main/java/com/example/njitpool/MainActivity.java
+++ app/src/main/java/com/example/njitpool/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.njitpool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button1;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.customerlogin);
+        button2 = findViewById(R.id.driverlogin);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCustomer();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDriver();
+            }
+        });
+    }
+
+    public void openCustomer() {
+        Intent intent = new Intent(this, CustomerLoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void openDriver() {
+        Intent intent = new Intent(this, DriverLoginActivity.class);
+        startActivity(intent);
     }
 }
